7b40806a98603d842c540fe4cd5b8c1063b226dd
Chris Lankford <christopherlankford@gmail.com>
2016-10-26 11:23:51 -0700

    Completed the redux actions using async action creators

--- public/actions.js
+++ public/actions.js
@@ -9,6 +9,47 @@ export const FETCH_PEOPLE_SUCCESS = 'FETCH_PEOPLE_SUCCESS';
 function fetchPeopleSuccess(people) {
     return {
         type: FETCH_PEOPLE_SUCCESS,
-        people
+        people,
+    };
+};
+
+export const SAVE_PEOPLE_REQUEST = 'SAVE_PEOPLE_REQUEST';
+function savePeopleRequest() {
+    return {
+        type: SAVE_PEOPLE_REQUEST,
+    };
+};
+
+export const SAVE_PEOPLE_FAILURE = 'SAVE_PEOPLE_FAILURE';
+function savePeopleFailure(error) {
+    return {
+        type: SAVE_PEOPLE_FAILURE,
+        error,
+    };
+};
+
+export const SAVE_PEOPLE_SUCCESS = 'SAVE_PEOPLE_SUCCESS';
+function savePeopleSuccess(people) {
+    return {
+        type: SAVE_PEOPLE_SUCCESS,
+        people,
+    };
+};
+
+export function fetchPeople() {
+    return function(dispatch) {
+        dispatch(fetchPeopleRequest());
+        apiClient.loadPeople().then((people) => {
+            dispatch(fetchPeopleSuccess(people));
+        });
+    };
+};
+
+export function savePeople(people) {
+    return function(dispatch) {
+        dispatch(savePeopleRequest());
+        apiClient.savePeople(people)
+            .then((resp) => { dispatch(savePeopleSuccess(people)) })
+            .catch((err) => { dispatch(savePeopleFailure(err)) })
     };
 };
