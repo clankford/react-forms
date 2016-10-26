d66ca75d65ba5deb466591283bd574092b437405
Chris Lankford <christopherlankford@gmail.com>
2016-10-26 11:16:16 -0700

    Added the implementation for the reducer

--- public/reducer.js
+++ public/reducer.js
@@ -4,3 +4,33 @@ const initialState = {
     saveStatus: 'READY',
     person: {},
 };
+
+export function reducer(state = initialState, action) {
+    switch(action.type) {
+        case FETCH_PEOPLE_REQUEST:
+            return Object.assign({}, state, {
+                isLoading: true,
+            });
+        case FETCH_PEOPLE_SUCCESS:
+            return Object.assign({}, state, {
+                people: action.people,
+                isLoading: false,
+            });
+        case SAVE_PEOPLE_REQUEST:
+            return Object.assign({}, state, {
+                saveStatus: 'SAVING',
+            });
+        case SAVE_PEOPLE_FAILURE:
+            return Object.assign({}, state, {
+                saveStatus: 'ERROR',
+            });
+        case SAVE_PEOPLE_SUCCESS:
+            return Object.assign({}, state, {
+                people: action.people,
+                person: {},
+                saveStatus: 'SUCCESS',
+            });
+        default:
+            return state;
+    };
+};
